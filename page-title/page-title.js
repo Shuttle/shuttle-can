60c5a944ffc9dca02396fd13398072fb76cd9b0f
Eben <me@ebenroux.co.za>
2017-12-21 17:30:08 +0200

    - page-title

--- page-title/page-title.js
+++ page-title/page-title.js
@@ -4,9 +4,15 @@ import view from './page-title.stache!';
 import i18n from '../infrastructure/i18n';
 
 export const ViewModel = DefineMap.extend({
+    localize: {
+        type: 'boolean',
+        value: true
+    },
     title: {
+        type: 'string',
+        value: '',
         get: function(value) {
-            return i18n.value(value);
+            return this.localize ? i18n.value(value) : value;
         }
     }
 });
